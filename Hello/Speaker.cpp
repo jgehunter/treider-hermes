abaddb5045295dd78bf1d4d5ea7019325f75b6a4
Jorge Hunter <jgehunter@gmail.com>
2021-09-15 19:32:53 +0200

    feat: hello world CMake

---
+++ Hello/Speaker.cpp
@@ -0,0 +1,12 @@
+#include "Speaker.h"
+
+using namespace Hello;
+using namespace std;
+
+namespace Hello
+{
+    void Speaker::sayHello()
+    {
+        cout << "Hello, world!\n";
+    }
+}
---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <Speaker.h>
+
+using namespace std;
+using namespace Hello;
+
+int main(int argc, char *argv[])
+{
+    Speaker *speaker = new Speaker();
+
+    speaker->sayHello();
+}
